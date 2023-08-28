8de77c552e536a2b571f72e618297e0965c5c8e7
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-28 17:34:48 +0300

    Make a functioning npm project

--- index.js
+++ index.js
@@ -12,29 +12,49 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = 'mongodb://ruiyot:Database1234@ac-zlcsago-shard-00-00.gyekfjv.mongodb.net:27017,ac-zlcsago-shard-00-01.gyekfjv.mongodb.net:27017,ac-zlcsago-shard-00-02.gyekfjv.mongodb.net:27017/bloglist?ssl=true&replicaSet=atlas-epehav-shard-0&authSource=admin&retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+app.get('/api/blogs', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
-    })
+    }).catch(err => next(err))
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
-    })
+    }).catch(err => next(err))
 })
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'MongoNotConnectedError') {
+    return response.status(409).send({error: 'Please refresh'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3003
 app.listen(PORT, () => {
