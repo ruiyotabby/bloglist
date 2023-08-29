aa5f3155950db5a148e4a825ff2a8ff7192b6e10
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-29 16:18:46 +0300

    Refactor into separate modules

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const config = require('./utils/config')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI).then(() => {
+  logger.info('connected to mongodb')
+}).catch(err => {
+  logger.error('error connecting to mongodb', err.message)
+})
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,62 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://ruiyot:Database1234@ac-zlcsago-shard-00-00.gyekfjv.mongodb.net:27017,ac-zlcsago-shard-00-01.gyekfjv.mongodb.net:27017,ac-zlcsago-shard-00-02.gyekfjv.mongodb.net:27017/bloglist?ssl=true&replicaSet=atlas-epehav-shard-0&authSource=admin&retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    }).catch(err => next(err))
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    }).catch(err => next(err))
-})
-
-const errorHandler = (error, request, response, next) => {
-  if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
-  } else if (error.name === 'MongoNotConnectedError') {
-    return response.status(409).send({error: 'Please refresh'})
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
