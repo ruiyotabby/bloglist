fa134e9206ba318f71abbcc466b66436bb32eaf6
Ruiyot Abby <ruiyotabby@gmail.com>
2023-09-02 00:16:11 +0300

    Add tests for the post and toJson methods

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 const Blog = require('../models/blog');
 const api = supertest(app)
 
-const initialBlog = [
+const initialBlogs = [
   {
     title: "First",
     author: "mwenyewe",
@@ -20,7 +20,7 @@ const initialBlog = [
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let blogObjects = initialBlog.map(blog => new Blog(blog))
+  let blogObjects = initialBlogs.map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
 })
@@ -35,7 +35,7 @@ test('should return blogs as json', async () => {
 test('should return all blogs', async () => {
   const returnedBlogs = await api.get('/api/blogs')
 
-  expect(returnedBlogs.body).toHaveLength(initialBlog.length)
+  expect(returnedBlogs.body).toHaveLength(initialBlogs.length)
 })
 
 test('a specific title is within the returned blogs', async () => {
@@ -43,6 +43,27 @@ test('a specific title is within the returned blogs', async () => {
   const titles = returnedBlogs.body.map(blog => blog.title)
 
   expect(titles).toContain('First')
+})
+
+test('unique identifier is named id', async () => {
+  const returnedBlogs = await api.get('/api/blogs')
+
+  expect(returnedBlogs.body[0].id).toBeDefined()
+})
+
+test('new blog post is saved to db', async () => {
+  const newBlog = {
+    title: "Third",
+    author: "hakuna",
+    likes: 99
+  }
+  const blog = new Blog(newBlog)
+  await blog.save()
+  const blogs = await api.get('/api/blogs')
+  const titles = blogs.body.map(b => b.title)
+
+  expect(blogs.body).toHaveLength(initialBlogs.length+1)
+  expect(titles).toContain('Third')
 })
 
 afterAll(async () => await mongoose.connection.close())
