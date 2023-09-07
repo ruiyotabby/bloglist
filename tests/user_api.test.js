a0b519bf88abf478b3be6c18c9cd2f4a37ae4023
Ruiyot Abby <ruiyotabby@gmail.com>
2023-09-07 14:25:14 +0300

    Add validation tests

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -6,6 +6,14 @@ const mongoose = require('mongoose')
 
 beforeEach( async () => {
   await User.deleteMany({})
+
+  const initialNote = new User({
+    name: 'John',
+    username: 'john1',
+    password: 'john2023'
+  })
+
+  await initialNote.save()
 })
 
 test('all users are returned', async () => {
@@ -19,25 +27,86 @@ test('all users are returned', async () => {
 
 })
 
-test('when a valid user is added', async () => {
-  const usersAtStart = await User.find({})
+describe('a new user', () => {
+  test('is added when valid', async () => {
+    const usersAtStart = await User.find({})
 
-  const user = {
-    name: 'root',
-    username: 'root',
-    password: '123'
-  }
+    const user = {
+      name: 'root',
+      username: 'root',
+      password: '123'
+    }
 
-  await api
-    .post('/api/users')
-    .send(user)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/users')
+      .send(user)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const usersAtEnd = await User.find({})
+    const usersAtEnd = await User.find({})
 
-  expect(usersAtEnd.map(u => u.passwordHash)).not.toContain('123')
-  expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+    expect(usersAtEnd.map(u => u.passwordHash)).not.toContain('123')
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+  })
+
+  test('returns 400 when a user without password is added', async () => {
+    const usersAtStart = await User.find({})
+
+    const user = {
+      name: 'root',
+      username: 'root'
+    }
+
+    await api
+      .post('/api/users')
+      .send(user)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await User.find({})
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('fails if a user with a short password is added', async () => {
+    const usersAtStart = await User.find({})
+
+    const user = {
+      name: 'root',
+      username: 'root',
+      password: '1'
+    }
+
+    await api
+      .post('/api/users')
+      .send(user)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await User.find({})
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('fails when a user with a duplicate username is added', async () => {
+    const usersAtStart = await User.find({})
+
+    const user = {
+      name: 'John',
+      username: 'john1',
+      password: 'john2023'
+    }
+
+    await api
+      .post('/api/users')
+      .send(user)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await User.find({})
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
 })
 
 afterAll(async ()=> await mongoose.connection.close())
